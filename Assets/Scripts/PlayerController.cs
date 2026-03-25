f4d347d30346344b0b10d3e560b2a8a61876eb09
evekrainikbradleyu <ekrainik@mail.bradley.edu>
2026-03-25 10:13:37 -0500

    scaffolds v1
    adds scaffolds, no placement system yet.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using NaughtyAttributes;
 using System.Runtime.CompilerServices;
+using System.Linq.Expressions;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
@@ -25,13 +27,20 @@ public class PlayerController : MonoBehaviour
     private MapController mapController;
     private bool movingPlayer;
     private GameObject currentSpace;
+    private bool onLadder;
+    private InputAction interact;
+    private int playerYLayer;
+    private bool interactOutput;
+    private bool playerOnSolidGround;
+    private InputAction leftClick;
 
     // serialized privates
-    [InfoBox("move speed = time taken to move between squares; less is faster"
-        )][SerializeField] private float moveSpeed;
+    [InfoBox("move speed = time taken to move between squares; less is faster")
+        ][SerializeField] private float moveSpeed;
     [SerializeField] private MapShellController mapShellController;
     [SerializeField] private GameObject playerStartSpace;
     [SerializeField] private CameraController cameraController;
+    [SerializeField] private ScaffoldingController scaffoldingController;
 
     #endregion
 
@@ -42,14 +51,21 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         move = InputSystem.actions.FindAction("Move");
+        interact = InputSystem.actions.FindAction("Interact");
+        leftClick = InputSystem.actions.FindAction("Left Click");
 
         move.performed += MovePerformed;
         move.canceled += MoveCanceled;
+        interact.performed += InteractPerformed;
+        interact.canceled += InteractCanceled;
+        leftClick.performed += LeftClickPerformed;
 
         mapController = mapShellController.shellController;
         movingPlayer = false;
         moveOutput = Vector2.zero;
         currentSpace = playerStartSpace;
+        onLadder = false;
+        playerYLayer = 0;
 
         transform.position = new Vector3
         (
@@ -59,39 +75,34 @@ public class PlayerController : MonoBehaviour
         );
     }
 
+
+
+    /// <summary>
+    /// checks for input and starts movement routines + interactions based upon 
+    /// that input
+    /// </summary>
     private void Update()
     {
         //Debug.Log("Move output: " + moveOutput.ToString());
+        //Debug.Log("Interact output: " + interactOutput.ToString());
+        //Debug.Log("On ladder: " + onLadder.ToString());
 
-        if (moveOutput != Vector2.zero && !movingPlayer)
-        {
-            GameObject nextSpace = null;
-
-            Vector2 fixedMoveOutput = moveOutput.x == 0 ? moveOutput.y > 0 ? 
-                new Vector2(0, 1) : new Vector2(0, -1) : moveOutput.x > 0 ? new 
-                Vector2(1, 0) : new Vector2(-1, 0);
-
-            fixedMoveOutput *= cameraController.nearestWall > 2 ? -1 : 1;
-            fixedMoveOutput = cameraController.nearestWall % 2 == 0 ? new 
-                Vector2(-fixedMoveOutput.y, fixedMoveOutput.x) : fixedMoveOutput
-                ;
-
-            nextSpace = mapController.GetSpaceFromVector(mapController.
-                GetVectorFromSpace(currentSpace) + fixedMoveOutput);
-
-            Debug.Log("next space vector:" + (mapController.GetVectorFromSpace(currentSpace) + fixedMoveOutput).ToString());
-            
-            if (nextSpace != null)
-            {
-                StartCoroutine(MovePlayer(nextSpace, moveSpeed));
-            }
-        }
+        UpdatePlayerOnSolidGround();
+        DoFalling();
+        StartInputMovements();
+        StartInteractions();
     }
 
     #endregion
 
     #region coroutines
 
+    /// <summary>
+    /// moves the player to the given space over the given amount of time
+    /// </summary>
+    /// <param name="destination">space to move to</param>
+    /// <param name="moveTime">how long it takes to move</param>
+    /// <returns></returns>
     private IEnumerator MovePlayer(GameObject destination, float moveTime)
     {
         Vector3 destinationPosition = destination.transform.position;
@@ -113,6 +124,7 @@ public class PlayerController : MonoBehaviour
             timeElapsed += Time.deltaTime;
             yield return null;
         }
+
         transform.position = new Vector3(destinationPosition.x, transform.
             position.y, destinationPosition.z);
         movingPlayer = false;
@@ -121,19 +133,222 @@ public class PlayerController : MonoBehaviour
         yield return null;
     }
 
+    /// <summary>
+    /// moves the player upwards
+    /// </summary>
+    /// <param name="spaces">how many spaces to move up</param>
+    /// <param name="moveTime">how long it takes to move</param>
+    /// <returns></returns>
+    private IEnumerator MoveUp(int spaces, float moveTime)
+    {
+        movingPlayer = true;
+        playerYLayer += spaces;
+        float timeElapsed = 0;
+
+        while (timeElapsed < moveTime)
+        {
+            transform.position = new Vector3
+            (
+                transform.position.x,
+                Mathf.Lerp(mapController.MapLayerHeights[playerYLayer - spaces]
+                    , mapController.MapLayerHeights[playerYLayer], timeElapsed 
+                    / moveTime),
+                transform.position.z
+            );
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = new Vector3(transform.position.x, mapController.
+            MapLayerHeights[playerYLayer], transform.position.z);
+        movingPlayer = false;
+
+        yield return null;
+    }
+
+    #endregion
+
+    #region miscellaneous functions
+
+    /// <summary>
+    /// checks to make sure move output is there and the player isnt already 
+    /// moving, then moves the player to the next space based on the move 
+    /// output.
+    /// </summary>
+    private void StartInputMovements()
+    {
+        // stops function if player is moving or there isnt move input
+        if (moveOutput == Vector2.zero || movingPlayer) { return; }
+
+
+        GameObject nextSpace = null;
+
+        Vector2 fixedMoveOutput = moveOutput.x == 0 ? moveOutput.y > 0 ?
+            new Vector2(0, 1) : new Vector2(0, -1) : moveOutput.x > 0 ? new
+            Vector2(1, 0) : new Vector2(-1, 0);
+
+        fixedMoveOutput *= cameraController.nearestWall > 2 ? -1 : 1;
+        fixedMoveOutput = cameraController.nearestWall % 2 == 0 ? new
+            Vector2(-fixedMoveOutput.y, fixedMoveOutput.x) :
+            fixedMoveOutput;
+
+        nextSpace = mapController.GetSpaceFromVector(mapController.
+            GetVectorFromSpace(currentSpace) + fixedMoveOutput);
+
+        /*Debug.Log("next space vector:" + (mapController.
+        GetVectorFromSpace(currentSpace) + fixedMoveOutput).ToString());*/
+
+        if (nextSpace == null) { return; }
+
+        StartCoroutine(MovePlayer(nextSpace, moveSpeed));
+
+    }
+
+    /// <summary>
+    /// starts interactions. interactions currently implemented include: 
+    /// ladders
+    /// </summary>
+    private void StartInteractions()
+    {
+        if (movingPlayer) { return; }
+
+        if (interactOutput)
+        {
+            if (onLadder)
+            {
+                StartCoroutine(MoveUp(1, moveSpeed));
+            }
+        }
+    }
+
+    /// <summary>
+    /// checks to make sure the player isn't standing on nothing
+    /// </summary>
+    private void UpdatePlayerOnSolidGround()
+    {
+        if (scaffoldingController.map.SolidGroundMap == null) { return; }
+
+        playerOnSolidGround = scaffoldingController.map.SolidGroundMap  
+            [playerYLayer]
+            [(int)mapShellController.shellController.GetVectorFromSpace(
+                currentSpace).x]
+            [(int)mapShellController.shellController.GetVectorFromSpace(
+                currentSpace).y];
+    }
+
+    private void DoFalling()
+    {
+        if (movingPlayer || playerOnSolidGround) { return; }
+
+        StartCoroutine(MoveUp(-1, moveSpeed));
+    }    
+
+    private void OnClick()
+    {
+        Debug.Log("Clicked");
+
+        if (EventSystem.current.IsPointerOverGameObject()) { return; }
+
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue
+            ());
+        RaycastHit hit;
+
+        if (Physics.Raycast (ray, out hit))
+        {
+            if (hit.collider.CompareTag("ScaffoldSpot"))
+            {
+                scaffoldingController.PlaceScaffolding
+                (
+                    0,
+                    new Vector3
+                    (
+                    0,
+                    mapController.GetVectorFromSpace(hit.collider.gameObject).x,
+                    mapController.GetVectorFromSpace(hit.collider.gameObject).y
+                    )
+                );
+            }
+        }
+    }
+
+    #endregion
+
+    #region other monobehaviour functions
+
+    /// <summary>
+    /// detects when the player is on special spaces
+    /// </summary>
+    /// <param name="other">space collider</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ladder"))
+        {
+            onLadder = true;
+        }
+    }
+
+    /// <summary>
+    /// detects when the player leaves special spaces
+    /// </summary>
+    /// <param name="other">space collider</param>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ladder"))
+        {
+            onLadder = false;
+        }
+    }
+
     #endregion
 
     #region input functions
 
+    /// <summary>
+    /// detects when the player stops move input
+    /// </summary>
+    /// <param name="obj">input ctx</param>
     private void MoveCanceled(InputAction.CallbackContext obj)
     {
         moveOutput = Vector2.zero;
     }
 
+    /// <summary>
+    /// detects when the player performs move input
+    /// </summary>
+    /// <param name="obj">input ctx</param>
     private void MovePerformed(InputAction.CallbackContext obj)
     {
         moveOutput = obj.ReadValue<Vector2>();
         moveOutput = new Vector2 (moveOutput.y, moveOutput.x);
+    }
+
+    /// <summary>
+    /// detects when the player releases the space bar
+    /// </summary>
+    /// <param name="obj">input ctx</param>
+    private void InteractCanceled(InputAction.CallbackContext obj)
+    {
+        interactOutput = false;
+    }
+
+    /// <summary>
+    /// detects when the player presses the space bar
+    /// </summary>
+    /// <param name="obj">input ctx</param>
+    private void InteractPerformed(InputAction.CallbackContext obj)
+    {
+        interactOutput = true;
+    }
+
+    /// <summary>
+    /// detects when the player clicks the left mouse button
+    /// </summary>
+    /// <param name="obj">input ctx</param>
+    private void LeftClickPerformed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Left Click Detected");
+        OnClick();
     }
 
     #endregion
---
+++ Assets/Scripts/ScaffoldingController.cs
@@ -0,0 +1,100 @@
+using System;
+using UnityEngine;
+
+public class ScaffoldingController : MonoBehaviour
+{
+    public ScaffoldMap map;
+
+    private MapController mapController;
+
+    [SerializeField] private MapShellController mapShellController;
+    [SerializeField] private GameObject[] scaffoldTypes;
+
+    private void Start()
+    {
+        mapController = mapShellController.shellController;
+
+        map = new ScaffoldMap(mapController);
+
+        PlaceScaffolding(0, new Vector3(0, 5, 5));
+        PlaceScaffolding(0, new Vector3(1, 5, 5));
+        PlaceScaffolding(0, new Vector3(2, 5, 5));
+    }
+    
+    /// <summary>
+    /// places a piece of scaffolding.
+    /// </summary>
+    /// <param name="scaffoldType">the type of scaffolding to place; available
+    /// types currently include: 0 - ladder</param>
+    /// <param name="position">the position on the map to place the scaffolding
+    /// ; x = height, y = row, z = col</param>
+    public void PlaceScaffolding(int scaffoldType, Vector3 position)
+    {
+        map.ScaffoldingPlacements[(int)position.x][(int)position.y][(int)
+            position.z] = Instantiate(scaffoldTypes[scaffoldType]);
+        map.ScaffoldingPlacements[(int)position.x][(int)position.y][(int)
+            position.z].transform.position = new Vector3
+            (
+                mapController.GetSpaceFromVector(new Vector2(position.y,
+                    position.z)).transform.position.x,
+                mapController.MapLayerHeights[(int)position.x],
+                mapController.GetSpaceFromVector(new Vector2(position.y,
+                    position.z)).transform.position.z
+            );
+
+        // checks to see if scaffold type is 1x1, update when new types added
+        if (Array.Exists<int>(new int[] { 0 }, i => i == scaffoldType))
+        {
+            if (position.x < mapController.MapHeight - 1)
+            {
+                map.ToggleSolidGround(new Vector3
+                    (
+                        position.x + 1,
+                        position.y,
+                        position.z
+                    ));
+            }
+        }
+    }
+}
+
+public class ScaffoldMap
+{
+    private bool[][][] solidGroundMap;
+    private GameObject[][][] scaffoldingPlacements;
+
+    public ScaffoldMap(MapController mapController)
+    {
+        solidGroundMap = new bool[mapController.MapHeight][][];
+        scaffoldingPlacements = new GameObject[mapController.MapHeight][][];
+        for (int i = 0;  i < solidGroundMap.Length; i++)
+        {
+            solidGroundMap[i] = new bool[mapController.Rows.Count][];
+            scaffoldingPlacements[i] = new GameObject[mapController.Rows.Count][];
+            for (int j = 0; j < solidGroundMap[i].Length; j++)
+            {
+                solidGroundMap[i][j] = new bool[mapController.Spaces[0].Count];
+                scaffoldingPlacements[i][j] = new GameObject[mapController.Spaces[0].Count];
+                for (int k = 0; k <  solidGroundMap[i][j].Length; k++)
+                {
+                    solidGroundMap[i][j][k] = i == 0; // makes floor solid and everything else fall-through
+                    scaffoldingPlacements[i][j][k] = null;
+                }
+            }
+        }
+    }
+
+    public void ToggleSolidGround(Vector3 position)
+    {
+        SolidGroundMap[(int)position.x][(int)position.y][(int)position.z] = 
+            !SolidGroundMap[(int)position.x][(int)position.y][(int)position.z];
+    }
+
+    /// <summary>
+    /// map of whether or not each space's ground is solid. first value = 
+    /// height, second value = row, third value = column
+    /// </summary>
+    public bool[][][] SolidGroundMap { get => solidGroundMap; set => 
+            solidGroundMap = value; }
+    public GameObject[][][] ScaffoldingPlacements { get => scaffoldingPlacements; set => scaffoldingPlacements = value; }
+}
